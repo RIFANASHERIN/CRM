e4e2d523978e4e6421c3f0d92bfd9d15701d2f8f
Rifana Sherin <rifanasherin44@gmail.com>
2023-05-10 09:40:31 +0530

    first commit

---
+++ leads/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    is_organiser = models.BooleanField(default=True)
+    is_agent = models.BooleanField(default=False)
+
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    agent = models.ForeignKey("Agent",null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category",related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.user.email
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=30) # New, Contacted, Converted, Unconverted
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    print(instance, created)
+    if created:
+        UserProfile.objects.create(user = instance)
+
+
+post_save.connect(post_user_created_signal, sender=User)
