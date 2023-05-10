b05fe59562e2759d64078d6cb7f1ddabe023169c
Rifana Sherin <rifanasherin44@gmail.com>
2023-05-10 14:20:28 +0530

    changed something

--- leads/urls.py
+++ leads/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     lead_list, lead_detail, lead_create, lead_update, lead_delete,
     LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView, 
-    AssignAgentView, CategoryListView, CategoryDetailView
+    AssignAgentView, CategoryListView, CategoryDetailView, CategoryUpdateView
 )
 
 app_name = "leads"
@@ -14,6 +14,8 @@ urlpatterns = [
     path('<int:pk>/update',LeadUpdateView.as_view(), name='lead-update'),
     path('<int:pk>/delete',LeadDeleteView.as_view(), name='lead-delete'),
     path('<int:pk>/assign-agent/',AssignAgentView.as_view(), name='assign-agent'),
+    path('<int:pk>/category/',CategoryUpdateView.as_view(), name='lead-category-update'),
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+
 ]
--- leads/views.py
+++ leads/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import Lead, Agent, Category
-from .forms import LeadForm, LeadModelForm, CustomUserCreationForm, AssignAgentForm
+from .forms import LeadForm, LeadModelForm, CustomUserCreationForm, AssignAgentForm, LeadCategoryUpdateForm
 from django.views import generic
 from django.core.mail import send_mail
 from agents.mixins import OrganisorAndLoginRequiredMixin
@@ -294,7 +294,23 @@ class CategoryDetailView(LoginRequiredMixin, generic.DetailView):
                 )
         return queryset 
 
-    
+class CategoryUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "leads/lead_category_update.html"
+    form_class = LeadCategoryUpdateForm
+
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_organiser:
+            queryset = Lead.objects.filter(organisation=user.userprofile)
+        else:
+            queryset = Lead.objects.filter(organisation=user.agent.organisation)
+        # filter for the agent that is logged in
+            queryset = queryset.filter(agent__user=user)
+        return queryset
+
+    def get_success_url(self):
+        return reverse("leads:lead-detail", kwargs={"pk":self.get_object().id})
 
 
 # def lead_update(request, pk):
